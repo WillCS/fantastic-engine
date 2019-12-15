3431f702e19ada0c91519b4a9360c2aafc8e268b
WillCS <wcstibbards@gmail.com>
2019-12-16 00:56:18 +1000

    Fixed modeller package in test, brought it up to scratch. I should write some tests soon.

---
+++ src/test/kotlin/dev/willcs/fantastic_engine/AppTest.kt
@@ -0,0 +1,11 @@
+package dev.willcs.fantastic_engine
+
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+// class AppTest {
+//     @Test fun testAppHasAGreeting() {
+//         val classUnderTest = ModelApp()
+//         assertNotNull(classUnderTest.greeting, "app should have a greeting")
+//     }
+// }
